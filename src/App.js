d206b64a116d5c425dc73a205b60d5c9258392ed
kjsau <101171603+kjsau@users.noreply.github.com>
2022-04-05 09:28:37 +0800

    added API integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import City from "./City";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <City />
+      <City defaultCity="London" />
     </div>
   );
 }
--- src/City.js
+++ src/City.js
@@ -1,91 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
-export default function City() {
-  let weatherData = {
-    city: "Perth",
-    temperature: 19,
-    date: "Tuesday 10:00",
-    description: "Cloudy",
-    humidity: 80,
-    wind: 3,
-  };
+export default function City(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  return (
-    <div className="container">
-      <div className="layout">
-        <br />
-        <div className="row">
-          <div className="col-6">
-            <div className="search-box">
-              <form>
-                <button className="btn-search" input type="submit">
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
-                <input
-                  type="text"
-                  className="input-search"
-                  placeholder="Enter a city"
-                  autoComplete="off"
-                />
-              </form>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="layout">
+          <div className="row">
+            <div className="col-6">
+              <div className="search-box">
+                <form>
+                  <button className="btn-search" input type="submit">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </button>
+                  <input
+                    type="text"
+                    className="input-search"
+                    placeholder="Enter a city"
+                    autoComplete="off"
+                  />
+                </form>
+              </div>
             </div>
-          </div>
-          <br />
-          <br />
+            <br />
+            <br />
 
-          <br />
+            <br />
 
-          <div className="clearfix">
-            <div className="cardMain">
-              <div className="card-header">
-                <h1>{weatherData.city}</h1>
-                <h2> {weatherData.date}</h2>
-                <div className="card-main">
-                  <i className="material-icons"></i>
-                  <div className="row">
-                    <div className="col-5">
-                      <ul>
-                        <li>
-                          <img
-                            src={require(`./Images/temperature.png`)}
-                            alt="temp"
-                            width="50px"
-                          />
-                          <h3> {weatherData.temperature}</h3>
-                        </li>
-                        <li>
-                          <img
-                            src={require(`./Images/windy.png`)}
-                            alt="wind"
-                            width="50px"
-                          />
-                          <h3> {weatherData.wind}</h3>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="col-5">
-                      <ul>
-                        <li>
-                          <img
-                            src={require(`./Images/weather-news.png`)}
-                            alt="describe"
-                            width="50px"
-                          />
-                          <h3> {weatherData.description} </h3>
-                        </li>
-                        <li>
-                          <img
-                            src={require(`./Images/humidity.png`)}
-                            alt="humid"
-                            width="50px"
-                          />
-                          <h3> {weatherData.humidity} </h3>
-                        </li>
-                      </ul>
+            <div className="clearfix">
+              <div className="cardMain">
+                <div className="card-header">
+                  <h1>{weatherData.city}</h1>
+                  <h2> </h2>
+                  <div className="card-main">
+                    <i className="material-icons"></i>
+                    <div className="row">
+                      <div className="col-5">
+                        <ul>
+                          <li>
+                            <img
+                              src={require(`./Images/temperature.png`)}
+                              alt="temp"
+                              width="50px"
+                            />
+                            <h3> {Math.round(weatherData.temperature)}</h3>
+                          </li>
+                          <li>
+                            <img
+                              src={require(`./Images/windy.png`)}
+                              alt="wind"
+                              width="50px"
+                            />
+                            <h3> {weatherData.wind}</h3>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="col-5">
+                        <ul>
+                          <li>
+                            <img
+                              src={require(`./Images/weather-news.png`)}
+                              alt="describe"
+                              width="50px"
+                            />
+                            <h3> {weatherData.description} </h3>
+                          </li>
+                          <li>
+                            <img
+                              src={require(`./Images/humidity.png`)}
+                              alt="humid"
+                              width="50px"
+                            />
+                            <h3> {weatherData.humidity} </h3>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -94,9 +101,10 @@ export default function City() {
           </div>
         </div>
       </div>
-      <a href="https://github.com/kjsau/weatherreactapp"> Open source code</a>
-      <span class="author"> by Katherine Scott</span>
-      <script src="source/app.js"></script>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "bd76e2bc6232f106529f184cc0d9a95d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+  }
 }
